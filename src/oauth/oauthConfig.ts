cc04e12887a902c85f33fc33cc3e96de1eff295e
sj0919 <seungjinlee09@gmail.com>
2026-01-03 21:22:42 +0900

    ✨feat : oauthConfig.ts 추가

--- src/oauth/oauthConfig.ts
+++ src/oauth/oauthConfig.ts
@@ -4,7 +4,7 @@ export const googleConfig: AuthConfiguration = {
   issuer: 'https://accounts.google.com',
   clientId:
     '60276299958-nn6jrsf78cj7as3dlrk9v745j211klgf.apps.googleusercontent.com',
-  redirectUrl: 'com.care.ride:/oauthredirect',
+  redirectUrl: 'com.careride:/oauthredirect',
   scopes: ['openid', 'email', 'profile'], // idToken 포함
 };
 export const oauthConfigMap: Record<'google', AuthConfiguration> = {
