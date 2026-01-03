e6feca25644f61a9d8d852430643dc9fdc27571b
sj0919 <seungjinlee09@gmail.com>
2026-01-03 21:57:04 +0900

    ✨feat : 날짜 계산 로직 추가

---
+++ src/utils/date.ts
@@ -0,0 +1,16 @@
+export const getCurrentYearMonth = () => {
+  const d = new Date();
+  const y = d.getFullYear();
+  const m = String(d.getMonth() + 1).padStart(2, '0');
+  return `${y}-${m}`;
+};
+
+export const addMonthsToYearMonth = (ym: string, delta: number) => {
+  const [yStr, mStr] = ym.split('-');
+  const y = Number(yStr);
+  const m = Number(mStr);
+  const d = new Date(y, m - 1 + delta, 1);
+  const yy = d.getFullYear();
+  const mm = String(d.getMonth() + 1).padStart(2, '0');
+  return `${yy}-${mm}`;
+};
