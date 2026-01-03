6c857fbe991de0e2345ad428e701c8482df1ccd3
sj0919 <seungjinlee09@gmail.com>
2026-01-03 22:02:15 +0900

    ♻️refactor : android 내부 파일 수정

--- android/app/src/main/java/com/carride/MainActivity.kt
+++ android/app/src/main/java/com/carride/MainActivity.kt
@@ -1,17 +1,31 @@
 package com.careride
 
+import android.os.Bundle
+import android.util.Log
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
+import com.kakao.sdk.common.util.Utility  
+import com.kakao.sdk.common.KakaoSdk
+import android.widget.Toast
+
 
 class MainActivity : ReactActivity() {
+
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
   override fun getMainComponentName(): String = "careride"
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+
+    val keyHash = Utility.getKeyHash(this)
+    Log.i("KAKAO_KEY_HASH", "keyHash = $keyHash")
+  }
 
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
